fd22f1f8e2d4538176b6e7f6aba0a934e0e8539f
Mayumi <mayumi.liyanage@gmail.com>
2015-02-24 22:57:18 -0500

    ParallelIO should wait until all the Futures completes

--- src/main/scala/com/example/BlockingIO.scala
+++ src/main/scala/com/example/BlockingIO.scala
@@ -2,27 +2,15 @@ package com.example
 
 import scala.concurrent._
 
-object FileIO {
+object BlockingIO {
   
   def main(args: Array[String]): Unit = {
-    
     performBlockingIO
-    
-    import ExecutionContext.Implicits.global
-    performParallelIO
   }
 
   import Utils._
   val LOOP_COUNT = 100000
-  
-  def performParallelIO(implicit context: ExecutionContext): Unit = {
-    timeElapsedInSeconds {
-      loopFor (LOOP_COUNT) {
-        Future(read).foreach(write)
-      }
-    }
-  }
-  
+
   def performBlockingIO: Unit = {
     timeElapsedInSeconds {
       loopFor (LOOP_COUNT) {
---
+++ src/main/scala/com/example/NonBlockingIO.scala
@@ -0,0 +1,34 @@
+package com.example
+
+import scala.io.Source._
+import java.io._
+import java.util.concurrent.atomic._
+import scala.concurrent._
+import scala.concurrent.forkjoin.ForkJoinPool
+import java.util.concurrent.TimeUnit
+
+object NonBlockingIO {
+  import Utils._
+  val LOOP_COUNT = 100000
+  
+  def main(args: Array[String]): Unit = {
+    val executor =  new ForkJoinPool()
+    implicit val context = ExecutionContext.fromExecutor(executor)
+
+    timeElapsedInSeconds {
+      loopFor (LOOP_COUNT) {
+        Future(read).foreach(write)
+      }
+      
+      try {
+        executor.shutdown()
+        executor.awaitTermination(1, TimeUnit.SECONDS)     
+      } catch {
+        case e: Exception => println("Something went wrong!")  
+      } 
+      
+    }
+    
+  }
+
+}
